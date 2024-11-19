e92908485b0b74af3d0abba3daed08f6e368bddd
sgotenks <pellerei.luca@gmail.com>
2024-11-19 17:52:49 +0100

    Create cards-speck.js

---
+++ blocks/cards-speck/cards-speck.js
@@ -0,0 +1,22 @@
+export default function decorate(block) {
+  const cols = [...block.firstElementChild.children];
+  block.classList.add(`columns-${cols.length}-cols`);
+
+  // setup image columns
+  [...block.children].forEach((row) => {
+    [...row.children].forEach((col) => {
+      const pic = col.querySelector('picture');
+      if (pic) {
+        const picWrapper = pic.closest('div');
+        if (picWrapper && picWrapper.children.length === 1) {
+          // picture is only content in column
+          picWrapper.classList.add('columns-img-col');
+        }
+      }
+      const temp = document.createElement('div');
+      temp.innerHTML = `<div class="sc-readMore">
+      <a href="#"><span>to the recipe</span></a></div>`;
+      col.append(temp.children.item(0));
+    });
+  });
+}
